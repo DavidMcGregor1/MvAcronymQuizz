f7fb9a23a1e15b3b9529e90f3ddad16bd38d30b1
David McGregor <david@plisky.net>
2023-04-29 16:15:52 +0100

    initial commit2

--- src/main/java/com/groupProject/MvAcronymQuizz2/MvAcronymQuizz2Application.java
+++ src/main/java/com/groupProject/MvAcronymQuizz2/MvAcronymQuizz2Application.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.groupProject.MvAcronymQuizz2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MvAcronymProjectApplication {
+public class MvAcronymQuizz2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MvAcronymProjectApplication.class, args);
+		SpringApplication.run(MvAcronymQuizz2Application.class, args);
 	}
 
 }
--- src/test/java/com/groupProject/MvAcronymQuizz2/MvAcronymQuizz2ApplicationTests.java
+++ src/test/java/com/groupProject/MvAcronymQuizz2/MvAcronymQuizz2ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.groupProject.MvAcronymQuizz2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MvAcronymProjectApplicationTests {
+class MvAcronymQuizz2ApplicationTests {
 
 	@Test
 	void contextLoads() {
